1b5951e0e66952cb14e097cbdcb2d363a7db7d50
User <User@PEAR-BROADCASTI>
2022-02-07 02:10:47 +0200

    Add project files.

---
+++ Scores/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scores.Models
+
+{
+    public class PersonModel
+    {
+  public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+   
+    }
+}
